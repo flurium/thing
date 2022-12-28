0a1d05a05da39714ea03ff085577d7bd6e87c496
Roman Koshchei <romankoshchey@gmail.com>
2022-12-28 20:38:15 +0200

    base

---
+++ Thing/Context/ThingDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using Thing.Models;
+
+namespace Thing.Context
+{
+    public class ThingDbContext : IdentityDbContext<User>
+    {
+        public ThingDbContext(DbContextOptions options) : base(options)
+        {
+            Database.EnsureCreated();
+        }
+    }
+}
---
+++ Thing/Repository/Interfaces/IRepository.cs
@@ -0,0 +1,11 @@
+﻿using System.Linq.Expressions;
+
+namespace Thing.Repository.Interfaces
+{
+    public interface IRepository<TEntity>
+    {
+        Task<IReadOnlyCollection<TEntity>> GetAllAsync();
+        Task<IReadOnlyCollection<TEntity>> FindByConditionAsync(Expression<Func<TEntity, bool>> conditon);
+        Task CreateAsync(TEntity entity);
+    }
+}
