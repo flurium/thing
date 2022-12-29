ecae65f2c8af360fa7b7e4feda1e5880d1e9f025
DmitryFlud2003 <valvech322@gmail.com>
2022-12-29 21:51:09 +0200

    Answer and Favorite Repositoties added

---
+++ Thing/Repository/AnswerRepository.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SendGrid.Helpers.Mail;
+using Thing.Context;
+using Thing.Models;
+using Thing.Repository.Interfaces;
+
+namespace Thing.Repository
+{
+    public class AnswerRepository : BaseRepository<Answer>, IAnswerRepository
+    {
+        public AnswerRepository(ThingDbContext context) : base(context)
+        {
+        }
+
+        public async Task Delete(int Id)
+        {
+            var answer = await Entities.FirstOrDefaultAsync(o => o.Id == Id).ConfigureAwait(false);
+            if (answer != null) Entities.Remove(answer);
+            _db.SaveChanges();
+        }
+
+        public async Task Edit(int Id, string Content)
+        {
+            var answer = await Entities.FirstOrDefaultAsync(o => o.Id == Id).ConfigureAwait(false);
+            if (answer != null) {
+                answer.Content = Content;
+                _db.Entry(answer).State = EntityState.Modified;
+                _db.SaveChanges();
+            }  
+        }
+    }
+}
