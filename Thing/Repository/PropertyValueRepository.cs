13bd1e813fcf59cc014dfbeb3ae35c2915636429
mariia-shcherbak <masha.shcherbak.2016@gmail.com>
2022-12-30 18:30:02 +0200

    PropertyValueRepository

---
+++ Thing/Repository/PropertyValueRepository.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Thing.Context;
+using Thing.Models;
+using Thing.Repository.Interfaces;
+
+namespace Thing.Repository
+{
+    public class PropertyValueRepository : BaseRepository<PropertyValue>, IPropertyValueRepository
+    {
+        public PropertyValueRepository(ThingDbContext context) : base(context)
+        {
+        }
+
+        public async Task Delete(int id)
+        {
+            var propertyValue = await Entities.FirstOrDefaultAsync(x => x.Id == id);
+            if (propertyValue != null) Entities.Remove(propertyValue);
+           
+        }
+    }
+}
