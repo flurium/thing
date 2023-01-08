f2be13a741d35464f4271928706d52c9f9de3171
Roman Koshchei <romankoshchei@gmail.com>
2023-01-08 10:46:30 +0200

    migrations to fix db

--- Thing/Models/Comment.cs
+++ Thing/Models/Comment.cs
@@ -8,10 +8,10 @@
         public string Content { get; set; }
 
         /// <summary> Good things about product </summary>
-        public string Pros { get; set; }
+        public string? Pros { get; set; }
 
         /// <summary> Bad things about product </summary>
-        public string Cons { get; set; }
+        public string? Cons { get; set; }
 
         public string? UserId { get; set; }
         public User? User { get; set; }
